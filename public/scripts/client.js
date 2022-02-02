3a5ec69ed9248cac1878f198ad3cc5251f4af845
Gerard Compion <gcompion93@gmail.com>
2022-02-01 20:14:01 -0500

    Function returns HTML data as desired

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,11 +8,11 @@ const tweetData = {
   "user": {
     "name": "Newton",
     "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
+    "handle": "@SirIsaac"
+  },
   "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
   "created_at": 1461116232227
 }
 
@@ -38,8 +38,26 @@ const createTweetElement = (tweetObj) => {
       timestamp = array[1];
     }
   }
-  console.log(name, avatar, handle, tweet, timestamp);
+
+  return `
+  <article class="tweet">
+    <header>
+      <div>
+        <img src=${avatar}>
+        <p>${name}</p>
+      </div>
+      <p class="handle">${handle}</p>
+    </header>
+    <p class="tweet-body">${tweet}</p>
+    <footer>
+      <p>${timestamp}</p>
+      <div>
+        <i class="fa-solid fa-flag"></i> <i class="fa-solid fa-retweet"></i> <i class="fa-solid fa-heart"></i>
+      </div>
+    </footer>
+  </article>`;
 };
 
-createTweetElement(tweetData);
+const $tweet = createTweetElement(tweetData);
+console.log($tweet);
 
