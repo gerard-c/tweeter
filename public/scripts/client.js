55254e185e43924f9b992e91fe94906032a41a1b
Gerard Compion <gcompion93@gmail.com>
2022-02-01 20:06:16 -0500

    Start of function to create HTML for tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,42 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
+
+const createTweetElement = (tweetObj) => {
+
+  let name = null;
+  let avatar = null;
+  let handle = null;
+  let tweet = null;
+  let timestamp = null;
+
+  const dataArrays = Object.entries(tweetObj);
+  for (const array of dataArrays) {
+    if (array[0] === 'user') {
+      name = array[1].name;
+      avatar = array[1].avatars;
+      handle = array[1].handle;
+    }
+    else if (array[0] === 'content') {
+      tweet = array[1].text;
+    } else {
+      timestamp = array[1];
+    }
+  }
+  console.log(name, avatar, handle, tweet, timestamp);
+};
+
+createTweetElement(tweetData);
+
